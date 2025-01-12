a7f00430aad9ed08b18f5bdd1fd87be8c22c93cc
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-01-12 16:18:52 +0530

    add slider section

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+
+export default function Slider() {
+  const [sliderPosition, setSliderPosition] = useState(60);
+
+  const handleSliderChange = (e) => {
+    setSliderPosition(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="w-full h-auto mt-16">
+        <h1 className="text-4xl font-bold uppercase text-center">
+          Remove Background Without <br /> Loosing Quality
+        </h1>
+        <div className="flex relative w-[800px] h-auto min-h-[450px] mx-auto rounded-3xl overflow-hidden
+        my-10">
+          {/* background image */}
+          <img
+            src={assets.image_w_bg}
+            style={{ clipPath: `inset(0 ${100.2 - sliderPosition}% 0 0)` }}
+            // 100.2 me se isliye minus kiya hai taaki right side se clipping ho
+            alt=""
+            className="absolute"
+          />
+          {/* foreground image */}
+          <img
+            src={assets.image_wo_bg}
+            style={{ clipPath: `inset(0 0 0 ${sliderPosition}%)` }}
+            // 100.2 me se isliye minus kiya hai taaki right side se clipping ho
+            alt=""
+            className="absolute"
+          />
+          <input
+            type="range"
+            name="slider"
+            value={sliderPosition}
+            onChange={handleSliderChange}
+            className="slider w-full my-auto z-30"
+            min={0}
+            max={100}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Card from "../components/Democards/Card";
 import Header from "../components/Header/Header";
+import Slider from "../components/Slider/Slider";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Card />
+      <Slider />
     </>
   );
 }
