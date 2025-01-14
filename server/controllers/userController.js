56a63af2b5c5e336b0805949ffaab1be926230d0
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-01-14 19:54:56 +0530

    fix folder structure for git

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+// API controller func - to manage clerk user with database
+// API endpoint - http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async (req,res) => {
+    // jab bhi ham frontend me koi user create karenge clerk se to vo backend document me bhi store ho
+    // clerk web hookks ke liye hame public ip chahiye jo ki vercel dega
+    
+}
