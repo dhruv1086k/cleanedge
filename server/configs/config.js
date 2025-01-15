24077e9579b22f5a805deae50af13d87a237b86b
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-01-15 16:04:36 +0530

    fix vercel public ip 1

--- server/configs/config.js
+++ server/configs/config.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("database connected");
-  });
-  await mongoose.connect(`${process.env.MONGODB_URI}/cleanedge`);
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("database connected");
+    });
+    await mongoose.connect(`${process.env.MONGODB_URI}/cleanedge`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    process.exit(1); // Exit the process if the database connection fails
+  }
 };
 
 export default connectDB;
